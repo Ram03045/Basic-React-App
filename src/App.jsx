ec5be9825aa990a0885f463f733e3f7aad50c1ce
unknown <Ram03045>
2024-04-28 19:50:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ProductTab from './ProductTab';
+import Button from './Button';
+
+
+function App() {
+  return (
+    <>
+      <h2>Blockbuster Deals | Shop Now</h2>
+      <ProductTab />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+function PrintHello(){
+    console.log("hello anjali and uma");
+}
+
+
+export default function Button(){
+    <div>
+        <button onClick={PrintHello}>Click me!</button>
+    </div>
+   
+}
